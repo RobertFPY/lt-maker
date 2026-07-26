a732c33dfbb8669602895f76dac22c547fbe48f6
RobertFPY <zzzchangngoxmaiyeuemzzz@gmail.com>
2026-07-26 08:15:18 +0700

    modified:   app/engine/general_states.py

--- app/engine/general_states.py
+++ app/engine/general_states.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, List, Literal, Optional, Tuple
+from typing import Callable, List, Optional, Tuple
 from collections import OrderedDict, defaultdict
 from enum import Enum
 
@@ -466,7 +466,6 @@ def battle_save():
 
 class OptionMenuState(MapState):
     name = 'option_menu'
-    keypress_history: List[Literal['DOWN', 'UP', 'LEFT', 'RIGHT']] = []
 
     def _populate_options(self) -> Tuple[List[str], List[str], List[bool], List[Optional[str]]]:
         """
@@ -540,7 +539,6 @@ class OptionMenuState(MapState):
         self.menu = menus.Choice(None, options, info=info_desc)
         self.menu.set_limit(9)
         self.menu.set_ignore(ignore)
-        self.keypress_history = []
 
     def begin(self):
         if game.memory.get('next_state') in ('objective_menu', 'settings_menu', 'base_guide', 'unit_menu'):
@@ -554,20 +552,6 @@ class OptionMenuState(MapState):
     def take_input(self, event):
         first_push = self.fluid.update()
         directions = self.fluid.get_directions()
-
-        if first_push:
-            if 'DOWN' in directions:
-                self.keypress_history.append('DOWN')
-            elif 'UP' in directions:
-                self.keypress_history.append('UP')
-            elif 'LEFT' in directions:
-                self.keypress_history.append('LEFT')
-            elif 'RIGHT' in directions:
-                self.keypress_history.append('RIGHT')
-            self.keypress_history = self.keypress_history[-8:]
-            if self.keypress_history == ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT']:
-                get_sound_thread().play_sfx('Select 1')
-                game.state.change('debug')
 
         self.menu.handle_mouse()
         if 'DOWN' in directions:
